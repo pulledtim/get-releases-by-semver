c431d492d45cc398b72cc99afa832aaa8839fb64
Stefan Wiedemann <wistefan@googlemail.com>
2021-05-25 11:12:50 +0200

    compile pattern

--- get-releases.py
+++ get-releases.py
@@ -6,12 +6,13 @@ import re
 repo = os.environ['REPOSITORY']
 pattern = os.environ['VERSION_PATTERN']
 
+regex = re.compile(pattern)
 github = Github()
 repository = github.get_repo(repo)
 matching_releases = []
 for release in repository.get_releases():
     print(release.title)
-    if pattern.match(release.title):
+    if regex.match(release.title):
         matching_releases.append(release)
 
 for v in matching_releases:
