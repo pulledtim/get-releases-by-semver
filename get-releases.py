58717ef55297f55302f2ccd1882be36abf5e2750
Stefan Wiedemann <wistefan@googlemail.com>
2021-05-25 10:55:53 +0200

    support pattern matching

--- get-releases.py
+++ get-releases.py
@@ -1,11 +1,17 @@
 from github import Github
 
 import os
+import re
 
 repo = os.environ['REPOSITORY']
 pattern = os.environ['VERSION_PATTERN']
 
 github = Github()
 repository = github.get_repo(repo)
+matching_releases = []
 for release in repository.get_releases():
-    print(release)
+    if re.match(pattern, release):
+        matching_releases.append(release)
+
+for v in matching_releases:
+    print(v)
